b45733d18d89f3584df9bd5330f8b22097895804
Tyler Hancock <darklime@live.ca>
2024-08-24 00:49:40 -0600

    Fix how update checker is resolved.

--- fabric/src/main/java/net/darkhax/pricklemc/fabric/impl/FabricMod.java
+++ fabric/src/main/java/net/darkhax/pricklemc/fabric/impl/FabricMod.java
@@ -1,12 +1,31 @@
 package net.darkhax.pricklemc.fabric.impl;
 
+import net.darkhax.pricklemc.common.impl.Constants;
 import net.darkhax.pricklemc.common.impl.PrickleMod;
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.DetectedVersion;
+
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class FabricMod implements ModInitializer {
 
     @Override
     public void onInitialize() {
         PrickleMod.getInstance().init();
+        checkForUpdates();
+    }
+
+    private static void checkForUpdates() {
+        try {
+            final HttpURLConnection connection = (HttpURLConnection) new URL("https://updates.blamejared.com/get?n=" + Constants.MOD_ID + "&gv=" + DetectedVersion.BUILT_IN.getName() + "&ml=fabric").openConnection();
+            connection.setRequestMethod("HEAD");
+            int responseCode = connection.getResponseCode();
+            if (responseCode != 200) {
+                Constants.LOG.warn("Version checker is not available.");
+            }
+        }
+        catch (Exception e) {
+        }
     }
 }
