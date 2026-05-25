4e7a27cf759de070e6f06f799a9adb39bcb686f3
Tyler Hancock <darklime@live.ca>
2026-05-24 18:24:20 -0600

    Move update checker to its own thread pool, and reduce timeout duration.

--- fabric/src/main/java/net/darkhax/pricklemc/fabric/PrickleMCFabric.java
+++ fabric/src/main/java/net/darkhax/pricklemc/fabric/PrickleMCFabric.java
@@ -7,19 +7,29 @@ import net.minecraft.DetectedVersion;
 import java.net.HttpURLConnection;
 import java.net.URI;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class PrickleMCFabric implements ModInitializer {
+
+    private static final ExecutorService UPDATE_EXECUTOR = Executors.newSingleThreadExecutor(r -> {
+        final Thread t = new Thread(r, "PrickleMC-Update-Checker");
+        t.setDaemon(true);
+        return t;
+    });
 
     @Override
     public void onInitialize() {
         PrickleMod.LOG.debug("Initializing Prickle.");
-        CompletableFuture.runAsync(PrickleMCFabric::checkForUpdates);
+        CompletableFuture.runAsync(PrickleMCFabric::checkForUpdates, UPDATE_EXECUTOR);
     }
 
     private static void checkForUpdates() {
         try {
             final HttpURLConnection connection = (HttpURLConnection) URI.create("https://updates.blamejared.com/get?n=" + PrickleMod.MOD_ID + "&gv=" + DetectedVersion.tryDetectVersion().name() + "&ml=fabric").toURL().openConnection();
             connection.setRequestMethod("HEAD");
+            connection.setConnectTimeout(5000);
+            connection.setReadTimeout(5000);
             int responseCode = connection.getResponseCode();
             if (responseCode != 200) {
                 PrickleMod.LOG.warn("Version checker is not available. Response code: {}", responseCode);
